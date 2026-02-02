92dfeeb93a1918411c646a8bf1e690cd326d095a
huzaifa baloch <huzaifa.baloch.9t@gmail.com>
2026-02-01 19:46:20 -0800

    update app

--- app.js
+++ app.js
@@ -5,22 +5,19 @@ const foodRoutes = require("../Backend/routes/foodRoute");
 
 const app = express();
 
-/* Middleware */
+
 app.use(cors());
 app.use(express.json());
 
-/* DB */
+
 connectDB();
 
-/* Routes */
 app.use("/api/foods", foodRoutes);
 
-/* Test route */
 app.get("/", (req, res) => {
   res.send("Restaurant API Running...");
 });
 
-/* Server */
 const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
